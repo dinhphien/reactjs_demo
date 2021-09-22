f2aeb4f3eb99d2f99be9ec9cd3c97a5c7aa42600
dinhphien <dinhquyphien1997@gmail.com>
2021-09-22 18:26:07 +0700

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { createBrowserHistory } from "history";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { Route } from "react-router";
+import createRootReducer from "./reducers";
 
+const history = createBrowserHistory();
+const store = createStore(
+  createRootReducer(history),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Route path="/" component={App} />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
