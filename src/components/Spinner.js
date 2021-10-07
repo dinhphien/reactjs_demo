42a832f4b98f71ac7c1eb5e2740a2208ca77471f
dinhphien <dinhquyphien1997@gmail.com>
2021-10-07 15:11:43 +0700

    add spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
-import { Spinner } from "react-bootstrap";
+import "../style/Spinner.css";
 export default class SpinnerCard extends Component {
   render() {
-    return (
-      <div className="card mb-3 mt-3 shadow-sm">
-        <div className="card-body">
-          <Spinner animation="border" variant="primary" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
-        </div>
-      </div>
-    );
+    return <div className="loader"></div>;
   }
 }
